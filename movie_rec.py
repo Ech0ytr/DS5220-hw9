81c70ef7ac4ae6dd8965a3177af4dda037e8a4e8
Ech0ytr <66861624+Ech0ytr@users.noreply.github.com>
2024-12-04 17:03:38 -0600

    Update movie_rec.py

--- movie_rec.py
+++ movie_rec.py
@@ -5,9 +5,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 movies = pd.read_csv('movies.csv')
 
-movies['name'] = movies['title'].str.extract(r'^(.*)\s\(\d{4}\)$')  
-movies['year'] = movies['title'].str.extract(r'\((\d{4})\)$')       
-movies['title'] = movies['name'] 
+movies['name'] = movies['title'].str.extract(r'^(.*)\s\(\d{4}\)$')
+movies['year'] = movies['title'].str.extract(r'\((\d{4})\)$')
+movies['title'] = movies['name']
 
 encoder = OneHotEncoder()
 genres_encoded = encoder.fit_transform(movies[['genres']]).toarray()
@@ -15,26 +15,23 @@ genres_encoded = encoder.fit_transform(movies[['genres']]).toarray()
 movies_features = genres_encoded
 
 def recommend_movies(movie_name, k=4):
-    try:
-        movie_index = movies[movies['title'] == movie_name].index[0]
-        
-        similarities = cosine_similarity([movies_features[movie_index]], movies_features)
-        
-        similar_indices = similarities.argsort()[0][-k-1:-1][::-1]
-        
-        recommendations = movies.iloc[similar_indices]['title'].tolist()
-        return recommendations
-    except IndexError:
+    if movie_name not in movies['title'].values:
         return []
+    movie_index = movies.index[movies['title'] == movie_name][0]
+    similarities = cosine_similarity([movies_features[movie_index]], movies_features)
+    similar_indices = similarities.argsort()[0][-k-1:-1][::-1]
+    recommendations = movies.iloc[similar_indices]['title'].tolist()
+    return recommendations
 
 st.title('Movie Recommendation System')
 
 movie_name = st.text_input('Enter a movie you like:')
 if st.button('Recommend'):
-    if movie_name in movies['title'].values:
-        recommendations = recommend_movies(movie_name)
+    recommendations = recommend_movies(movie_name)
+    if recommendations:
         st.write('Recommended Movies:')
         for rec in recommendations:
             st.write(f'- {rec}')
     else:
         st.write('Movie not found. Please try another title.')
+
