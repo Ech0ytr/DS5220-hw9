5705e6723687273bb1770b563723073b61f4a43b
Ech0ytr <66861624+Ech0ytr@users.noreply.github.com>
2024-12-04 17:26:17 -0600

    Update movie_rec.py

--- movie_rec.py
+++ movie_rec.py
@@ -3,41 +3,36 @@ import streamlit as st
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics.pairwise import cosine_similarity
 
-def load_and_preprocess_data():
-    movies = pd.read_csv('movies.csv')
-    movies['name'] = movies['title'].str.extract(r'^(.*)\s\(\d{4}\)$')  
-    movies['year'] = movies['title'].str.extract(r'\((\d{4})\)$')       
-    movies['title'] = movies['name']
-    encoder = OneHotEncoder()
-    genres_encoded = encoder.fit_transform(movies[['genres']]).toarray()
-    similarity_matrix = cosine_similarity(genres_encoded)
-    return movies, similarity_matrix
+movies = pd.read_csv('movies.csv')
 
-movies, similarity_matrix = load_and_preprocess_data()
+movies['name'] = movies['title'].str.extract(r'^(.*)\s\(\d{4}\)$')  
+movies['year'] = movies['title'].str.extract(r'\((\d{4})\)$')       
+movies['title'] = movies['name'] 
+
+encoder = OneHotEncoder()
+genres_encoded = encoder.fit_transform(movies[['genres']]).toarray()
+
+movies_features = genres_encoded
+
 
 def recommend_movies(movie_name, k=4):
-    if movie_name in movies['title'].values:
-        movie_index = movies[movies['title'] == movie_name].index[0]
-        similarity_scores = similarity_matrix[movie_index]
-        similar_indices = similarity_scores.argsort()[-k-1:-1][::-1]
-        recommendations = movies.iloc[similar_indices]['title'].tolist()
-        return recommendations
-    else:
-        return []
+    movie_index = movies[movies['title'] == movie_name].index[0]
+    
+    similarities = cosine_similarity([movies_features[movie_index]], movies_features)
+    
+    similar_indices = similarities.argsort()[0][-k-1:-1][::-1]
+    
+    recommendations = movies.iloc[similar_indices]['title'].tolist()
+    return recommendations
 
 st.title('Movie Recommendation System')
 
-movie_name = st.selectbox(
-    'Choose a movie you like:',
-    options=movies['title'].dropna().sort_values().tolist()
-)
-
+movie_name = st.text_input('Enter a movie you like:')
 if st.button('Recommend'):
-    recommendations = recommend_movies(movie_name)
-    if recommendations:
+    if movie_name in movies['title'].values:
+        recommendations = recommend_movies(movie_name)
         st.write('Recommended Movies:')
         for rec in recommendations:
             st.write(f'- {rec}')
     else:
-        st.write('No recommendations found.')
-
+        st.write('Movie not found. Please try another title.')
