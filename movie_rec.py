78a4766adc95847cd872facc08155f9b02cff550
Ech0ytr <66861624+Ech0ytr@users.noreply.github.com>
2024-12-04 18:17:25 -0600

    Update movie_rec.py

--- movie_rec.py
+++ movie_rec.py
@@ -1,38 +1,37 @@
+from sklearn.preprocessing import OneHotEncoder
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import streamlit as st
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics.pairwise import cosine_similarity
 
 movies = pd.read_csv('movies.csv')
-
-movies['name'] = movies['title'].str.extract(r'^(.*)\s\(\d{4}\)$')  
-movies['year'] = movies['title'].str.extract(r'\((\d{4})\)$')       
-movies['title'] = movies['name'] 
+movies['name'] = movies['title'].str.extract(r'^(.*)\s\(\d{4}\)$')
+movies['year'] = movies['title'].str.extract(r'\((\d{4})\)$')
+movies['title'] = movies['name']
 
 encoder = OneHotEncoder()
-genres_encoded = encoder.fit_transform(movies[['genres']]).toarray()
-
-movies_features = genres_encoded
-
+genres_encoded = csr_matrix(encoder.fit_transform(movies[['genres']]).toarray())
 
 def recommend_movies(movie_name, k=4):
+    if movie_name not in movies['title'].values:
+        return []
     movie_index = movies[movies['title'] == movie_name].index[0]
-    
-    similarities = cosine_similarity([movies_features[movie_index]], movies_features)
-    
-    similar_indices = similarities.argsort()[0][-k-1:-1][::-1]
-    
+    similarity_scores = cosine_similarity(genres_encoded[movie_index], genres_encoded).flatten()
+    similar_indices = similarity_scores.argsort()[-k-1:-1][::-1]
     recommendations = movies.iloc[similar_indices]['title'].tolist()
     return recommendations
 
 st.title('Movie Recommendation System')
-
 movie_name = st.text_input('Enter a movie you like:')
 if st.button('Recommend'):
     if movie_name in movies['title'].values:
         recommendations = recommend_movies(movie_name)
-        st.write('Recommended Movies:')
-        for rec in recommendations:
-            st.write(f'- {rec}')
+        if recommendations:
+            st.write('Recommended Movies:')
+            for rec in recommendations:
+                st.write(f'- {rec}')
+        else:
+            st.write('No recommendations found.')
     else:
         st.write('Movie not found. Please try another title.')
+
